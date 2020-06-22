fafb94345663efb60dc6a8e0f781770d3bbe1964
nicolewang2000 <nicole.wang@yale.edu>
2020-06-22 15:42:20 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Artist.delete_all
+Song.delete_all
+
+taylor_swift = Artist.create(name: "Taylor Swift")
+miley_cyrus = Artist.create(name: "Miley Cyrus")
+ed_sheeran = Artist.create(name: "Ed Sheeran")
+
+love_story = taylor_swift.songs.build(title: "Love Story").save
+twenty_two = taylor_swift.songs.build(title: "22").save
+this_love = taylor_swift.songs.build(title: "This Love").save
+the_climb = miley_cyrus.songs.build(title: "The Climb").save
+party_in_usa = miley_cyrus.songs.build(title: "Party in the USA").save
+a_team = ed_sheeran.songs.build(title: "A Team").save
